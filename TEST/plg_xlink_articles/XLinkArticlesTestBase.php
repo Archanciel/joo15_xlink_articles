9232ad1146bc2b1ee9b97ee7ea7847b3c1b8fa1b
Archanciel <jp.schnyder@gmail.com>
2014-03-30 01:14:33 +0100

    adapt base test to joo15 dev db and tested files location
    Now, tests ok in Eclipse Joomla 15 dev environment

--- TEST/plg_xlink_articles/XLinkArticlesTestBase.php
+++ TEST/plg_xlink_articles/XLinkArticlesTestBase.php
@@ -2,7 +2,7 @@
 
 require_once dirname ( __FILE__ ) . '\..\lib\TestCase.php';
 
-define('PLG_XLINK_ARTICLES_PATH', JPATH_BASE . '\plugins\content\xlink_articles');
+define('PLG_XLINK_ARTICLES_PATH', JPATH_BASE . '\plugins\content\plg_xlink_articles');
 
 /**
  *  Defining class as abstract is sementically correct and prevents MakeGood from executing
@@ -19,7 +19,7 @@ abstract class XLinkArticlesTestBase extends PHPUnit_Extensions_Database_TestCas
 // 		$dbURLPHP5_3_6_andLater = 'mysql:host=localhost;dbname=plucon15_dev;charset=UTF-8';
 // 		$pdo = new PDO ( $dbURLPHP5_3_6_andLater, 'root', '' );
 
-		$dbURLBeforePHP5_3_6 = 'mysql:host=localhost;dbname=plucon15_dev';
+		$dbURLBeforePHP5_3_6 = 'mysql:host=localhost;dbname=pluscon15_dev';
 		$options = array (PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8' );
 		$pdo = new PDO ( $dbURLBeforePHP5_3_6, 'root', '', $options );	// no pw for root !
 
