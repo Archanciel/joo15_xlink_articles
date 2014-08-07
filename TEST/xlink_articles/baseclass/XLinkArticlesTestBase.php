de1da18986572f3736c4bed0089e5c34561752be
Archanciel <jp.schnyder@gmail.com>
2014-08-07 23:11:57 +0200

    rename plg_xlink_articles -> xlink_articles

--- TEST/xlink_articles/baseclass/XLinkArticlesTestBase.php
+++ TEST/xlink_articles/baseclass/XLinkArticlesTestBase.php
@@ -2,7 +2,7 @@
 
 require_once dirname ( __FILE__ ) . '\..\..\lib\TestCase.php';
 
-define('PLG_XLINK_ARTICLES_PATH', JPATH_BASE . '\plugins\content\plg_xlink_articles');
+define('PLG_XLINK_ARTICLES_PATH', JPATH_BASE . '\plugins\content\xlink_articles');
 
 /**
  *  Defining class as abstract is sementically correct and prevents MakeGood from executing
